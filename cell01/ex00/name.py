c0da31948d4b477f0de9506ea1c0d9c7dbba6642
neymo-13 <cch.n.oop@gmail.com>
2026-02-09 13:08:21 +0700

    cell01

---
+++ cell01/ex00/name.py
@@ -0,0 +1,4 @@
+first_name = "Chutimar"
+last_name = "Opassamutchai"
+
+print(first_name,last_name)
---
+++ cell01/ex01/name.py
@@ -0,0 +1,5 @@
+first_name = "Chutimar"
+last_name = "Opassamutchai"
+
+whole_name = last_name + " " + first_name
+print(whole_name)
