5a3ceb8e09c5be765dcd145f6ba968fa618f38e8
neymo-13 <cch.n.oop@gmail.com>
2026-02-11 21:23:29 +0700

    cell03-indentation

--- cell03/ex00/to25.py
+++ cell03/ex00/to25.py
@@ -3,7 +3,7 @@
 num = int(input("Enter a number less than 25\n"))
 
 if num > 25:
-    print("Error")
+	print("Error")
 else:
-    for i in range(num, 26):
-        print(f"Inside the loop, my variable is {i}")
+	for i in range(num, 26):
+		print(f"Inside the loop, my variable is {i}")
--- cell03/ex02/i_got_that.py
+++ cell03/ex02/i_got_that.py
@@ -3,4 +3,4 @@
 user_input = input("What you gotta say? : ")
 
 while user_input != "STOP":
-    user_input = input("I got that! Anything else? : ")
+	user_input = input("I got that! Anything else? : ")
