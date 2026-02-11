7e3038093146cc694e6e3880c348c6437c0f7796
neymo-13 <cch.n.oop@gmail.com>
2026-02-11 21:24:12 +0700

    cell04-indentation

--- cell04/ex03/float.py
+++ cell04/ex03/float.py
@@ -3,6 +3,6 @@
 number = float(input("Give me a number: "))
 
 if number.is_integer():
-    print("This number is an integer.")
+	print("This number is an integer.")
 else:
-    print("This number is a decimal.")
+	print("This number is a decimal.")
